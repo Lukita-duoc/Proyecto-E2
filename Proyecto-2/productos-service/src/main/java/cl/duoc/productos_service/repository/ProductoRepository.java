e7b4b7a026f2c696cd1d839ff3cc4a9bd78eeebc
Lukita-duoc <luc.ortizr@duocuc.cl>
2026-05-13 00:16:34 -0400

    Delete Proyecto-2/productos-service directory

--- Proyecto-2/productos-service/src/main/java/cl/duoc/productos_service/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.duoc.productos_service.repository;
-
-import cl.duoc.productos_service.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
