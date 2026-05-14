36d5c6d075ea62683d327f97e925003ffe246a92
Lucas <luc.ortizr@duocuc.cl>
2026-05-14 19:34:16 -0400

    inventario-service falta probar en postman y poblar la tabla

---
+++ Proyecto-2/inventario-service/target/classes/db/migration/V2__poblar_tabla_inventario.sql
@@ -0,0 +1,18 @@
+INSERT INTO inventarios (stock_actual, stock_minimo, ubicacion, producto_id, sucursal_id) VALUES
+(100, 20, 'Bodega A-1', 1, 1),
+(45, 10, 'Pasillo 4', 2, 1),
+(15, 5, 'Vitrina Principal', 3, 1),
+
+(80, 15, 'Bodega Norte', 1, 2),
+(25, 10, 'Pasillo 2', 3, 2),
+
+(120, 30, 'Bodega General', 1, 3),
+(60, 15, 'Estante 05', 2, 3),
+(40, 10, 'Caja de Seguridad', 3, 3),
+
+(50, 10, 'Bodega Mar', 1, 4),
+(10, 5, 'Exhibición', 2, 4),
+
+(200, 40, 'Almacén Sur', 1, 5),
+(15, 20, 'Pasillo 1', 2, 5),
+(90, 25, 'Bodega B-2', 3, 5);
