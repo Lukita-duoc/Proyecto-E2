7930bb6f3f1622dfe4391f79fac5406d4dd00d3b
Kri <cristo.rojasv@duocuc.cl>
2026-05-15 22:38:54 -0400

    SIU

---
+++ Proyecto-2/facturacion-service/src/main/resources/db/migration/V2__poblar_tabla_facturacion.sql
@@ -0,0 +1,5 @@
+INSERT INTO facturas (fecha_emision, metodo_pago, total, id_cliente, id_orden) VALUES
+          ('2026-05-15', 'TARJETA DE DEBITO', 15000, 1, 1),
+          ('2026-05-15', 'TRANSFERENCIA', 45000, 2, 2),
+          ('2026-05-16', 'TARJETA DE CREDITO', 120000, 3, 3),
+          ('2026-05-16', 'EFECTIVO', 7500, 1, 4);
