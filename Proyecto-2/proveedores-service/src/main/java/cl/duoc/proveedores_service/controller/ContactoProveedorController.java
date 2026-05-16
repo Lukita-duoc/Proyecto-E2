59bc0166100b9b2c9a1e23b43b0ad76410888823
Lucas <luc.ortizr@duocuc.cl>
2026-05-15 21:21:40 -0400

    Falta validacion y detallar mas metodos

--- Proyecto-2/proveedores-service/src/main/java/cl/duoc/proveedores_service/controller/ContactoProveedorController.java
+++ Proyecto-2/proveedores-service/src/main/java/cl/duoc/proveedores_service/controller/ContactoProveedorController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/contactoproveedores")
+@RequestMapping("/api/v1/contactoproveedores")
 public class ContactoProveedorController {
 
     @Autowired
