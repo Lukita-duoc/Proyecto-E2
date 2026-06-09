7dec9f0271ec0a44f2aa2f337b8051abee36abaa
Kri <cristo.rojasv@duocuc.cl>
2026-06-09 02:23:52 -0400

    añadido la documentación en Envíos

--- Proyecto-2/envios-service/src/main/java/cl/duoc/envios_service/EnviosServiceApplication.java
+++ Proyecto-2/envios-service/src/main/java/cl/duoc/envios_service/EnviosServiceApplication.java
@@ -1,13 +1,31 @@
 package cl.duoc.envios_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Api de Envios",
+				version = "1.0.1",
+				description = "Documentación de API de Envios con todos los endpoints creados",
+				contact = @Contact(
+						name = "Equipo Rocket",
+						email = "cristo.rojasv@duocuc.cl"
+
+				)
+		)
+)
+
 public class EnviosServiceApplication {
 
 	public static void main(String[] args) {
@@ -15,3 +33,4 @@ public class EnviosServiceApplication {
 	}
 
 }
+
--- Proyecto-2/envios-service/src/main/java/cl/duoc/envios_service/controller/EnvioController.java
+++ Proyecto-2/envios-service/src/main/java/cl/duoc/envios_service/controller/EnvioController.java
@@ -3,16 +3,34 @@ package cl.duoc.envios_service.controller;
 import cl.duoc.envios_service.dto.EnvioDTO;
 import cl.duoc.envios_service.model.Envio;
 import cl.duoc.envios_service.service.EnvioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.ws.rs.Produces;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/*Errores:
+200: salió bien (OK) Relacionado con el GET
+201: Creado. Relacionado con el POST
+204: No hay contenido, asi es como se confirma que algo se borró y no hay nada. Relacionado con el DELETE
+404: BAD REQUEST (Quizás se introducieron malos datos)
+400: Datos incorrectamente colocados
+
+*/
+
 @RestController
 @RequestMapping("/api/v1/envios")
+@Tag(name = "Envios", description = "Controlador para el CRUD de Envios.")
+
 public class EnvioController {
 
     @Autowired
@@ -24,6 +42,16 @@ public class EnvioController {
         return ResponseEntity.ok(e);
     }
 
+    @Operation(
+            summary = "Buscar por ID el envío.",
+            description = "Método que busca un producto a través del ID (Long)"
+    )
+    @ApiResponse(responseCode = "200",
+            description = "Envío encontrado",
+            content = @Content(schema = @Schema(implementation = Produces.class)))
+    @ApiResponse(responseCode = "404",
+        description = "Envío no encontrado",
+        content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     @GetMapping("/{id}")
     public ResponseEntity<?> buscarId(@PathVariable Long id) {
         Envio e = envioService.findById(id);
@@ -31,12 +59,23 @@ public class EnvioController {
         return ResponseEntity.ok(e);
     }
 
+    @Operation(
+            summary = "Buscar por ID el detalle del envío.",
+            description = "Método que busca el detalle del envío a través del ID (Long)"
+    )
+    @ApiResponse(responseCode = "200",
+            description = "Detalle de envío encontrado",
+            content = @Content(schema = @Schema(implementation = Produces.class)))
+    @ApiResponse(responseCode = "404",
+        description = "Detalle de envío no encontrado",
+        content = @Content(schema = @Schema(implementation = ErrorResponse.class ) ))
     @GetMapping("/detalle/{id}")
     public ResponseEntity<EnvioDTO> buscarPorIdDto(@PathVariable Long id) {
         EnvioDTO dto = envioService.buscarDTO(id);
         if (dto == null) return ResponseEntity.notFound().build();
         return ResponseEntity.ok(dto);
     }
+
 
     @PostMapping
     public ResponseEntity<?> guardar(@Valid @RequestBody Envio envio) {
@@ -45,6 +84,19 @@ public class EnvioController {
         return new ResponseEntity<>(e, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Actualizar el envío.",
+            description = "Método que actualiza el envío a través del ID (Long)"
+    )
+    @ApiResponse(responseCode = "400",
+            description = "Datos de actualización inválidos",
+            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    @ApiResponse(responseCode = "404",
+            description = "Envío no encontrado para actualizar",
+            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    @ApiResponse(responseCode = "200",
+            description = "Envío actualizado correctamente",
+            content = @Content(schema = @Schema(implementation = Envio.class)))
     @PutMapping("/{id}")
     public ResponseEntity<Envio> actualizar(@PathVariable Long id, @Valid @RequestBody Envio envio) {
         Envio actualizado = envioService.update(id, envio);
@@ -52,6 +104,15 @@ public class EnvioController {
         return ResponseEntity.ok(actualizado);
     }
 
+    @Operation(
+            summary = "Eliminar por ID el envío.",
+            description = "Método que busca eliminar un envío a través del ID (Long)"
+    )
+    @ApiResponse(responseCode = "404",
+            description = "Producto no encontrado para eliminar",
+            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    @ApiResponse(responseCode = "204",
+            description = "Producto eliminado correctamente")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminar(@PathVariable Long id) {
         envioService.deleteById(id);
