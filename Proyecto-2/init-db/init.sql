4001a2ac666d1ac9873b62261b8aefa8b76b7e8d
Lucas-Duoc <luc.ortizr@duocuc.cl>
2026-06-12 16:28:27 -0400

    docker en rama prueba

---
+++ Proyecto-2/init-db/init.sql
@@ -0,0 +1,10 @@
+CREATE database if not exists bd_clientes;
+CREATE database if not exists bd_empleados;
+CREATE database if not exists bd_envios;
+CREATE database if not exists bd_facturacion;
+CREATE database if not exists bd_inventarios;
+CREATE database if not exists bd_ordenes;
+CREATE database if not exists bd_productos;
+CREATE database if not exists bd_proveedores;
+
+
